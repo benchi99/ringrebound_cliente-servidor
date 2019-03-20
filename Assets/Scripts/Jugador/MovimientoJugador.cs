9e9b860d6b6d8b0ff0dc0bd3eef27d3fd38594ce
Rubén Bermejo Romero <mejoruben@gmail.com>
2019-03-20 21:12:06 +0100

    Jugador implementado, comienzo implementación multijugador básico.

--- Assets/Scripts/Jugador/MovimientoJugador.cs
+++ Assets/Scripts/Jugador/MovimientoJugador.cs
@@ -4,7 +4,29 @@ using UnityEngine;
 
 public class MovimientoJugador : MonoBehaviour
 {
-    private float velocidad;
 
+    [SerializeField] private float velocidad = 5;
+    private CharacterController controlJugador;
+
+    private void Awake()
+    {
+        controlJugador = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        movimientoJugador();
+    }
+
+    void movimientoJugador()
+    {
+        float entradaVertical = Input.GetAxis("Vertical") * velocidad;
+        float entradaHorizontal = Input.GetAxis("Horizontal") * velocidad;
+
+        Vector3 movimientoHaciaDelante = transform.forward * entradaVertical;
+        Vector3 movimientoADerecha = transform.right * entradaHorizontal;
+
+        controlJugador.SimpleMove(movimientoHaciaDelante + movimientoADerecha);
+    }
 
 }
