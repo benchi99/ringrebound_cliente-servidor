93f5c866a0ad5595320a4c8f58573cc490223d9c
Rubén Bermejo Romero <mejoruben@gmail.com>
2019-04-09 23:46:05 +0200

    forgot to stage these files in last commit

--- Assets/Scripts/Menu/AccionesMenu.cs
+++ Assets/Scripts/Menu/AccionesMenu.cs
@@ -2,10 +2,54 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class AccionesMenu : MonoBehaviour
 {
-    
+
+    [SerializeField] private Canvas menu;
+
+    [SerializeField] private Button playButton;
+    [SerializeField] private Button disconnectButton;
+
+    void Awake()
+    {
+        disconnectButton.gameObject.SetActive(false);
+    }
+
+    private void OnLevelWasLoaded(int level)
+    {
+        if (level == 0)
+        {
+            playButton.gameObject.SetActive(true);
+            disconnectButton.gameObject.SetActive(false);
+        }
+        else if (level == 1)
+        {
+            menu.gameObject.SetActive(false);
+            playButton.gameObject.SetActive(false);
+            disconnectButton.gameObject.SetActive(true);
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && SceneManager.GetActiveScene().name == "Main")
+        {
+            menu.gameObject.SetActive(!menu.gameObject.activeSelf);
+
+            if (menu.gameObject.activeSelf)
+            {
+                Cursor.lockState = CursorLockMode.None;
+            }
+            else
+            {
+                Cursor.lockState = CursorLockMode.Locked;
+            }
+        }
+
+    }
+
     //Este método se llama cuando se quiere cerrar el juego.
     public void ExitGame()
     {
---
+++ Assets/Scripts/Utils/Mantenme.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mantenme : MonoBehaviour
+{
+    private static Mantenme _menu;
+    public static Mantenme Menu
+    {
+        get { return _menu; }
+    }
+
+    void Awake()
+    {
+        // Me aseguro de que solo hay un menú de pausa por instancia de juego.
+        if (_menu == null)
+        {
+            _menu = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
