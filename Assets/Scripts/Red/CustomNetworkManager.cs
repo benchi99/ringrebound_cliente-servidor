60ebbc1819a34edd1ba024b81100dc9f8c93323a
Rubén Bermejo Romero <mejoruben@gmail.com>
2019-04-18 13:09:36 +0200

    Commented CustomNetworkManager.cs

--- Assets/Scripts/Red/CustomNetworkManager.cs
+++ Assets/Scripts/Red/CustomNetworkManager.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 
+/// <summary>
+/// Clase que extiende a NetworkManager, para que
+/// mi interfaz pueda interactuar con el mismo
+/// NetworkManager.
+/// </summary>
 public class CustomNetworkManager : NetworkManager
 {
     private string serverName;
 
+    /// <summary>
+    /// Aloja una partida del juego.
+    /// </summary>
     public void EmpezarHost()
     {
         if (serverName == null || serverName == "")
@@ -17,26 +25,44 @@ public class CustomNetworkManager : NetworkManager
         print("Partida " + serverName + " iniciada.");
     }
 
+    /// <summary>
+    /// Se conecta a una partida.
+    /// </summary>
     public void ConectarAPartida()
     {
         base.StartClient();
     }
 
+    /// <summary>
+    /// Establece la ip.
+    /// </summary>
+    /// <param name="ip">La ip.</param>
     public void EstablecerIP(string ip)
     {
         networkAddress = ip;
     }
 
+    /// <summary>
+    /// Establece el puerto.
+    /// </summary>
+    /// <param name="puerto">El puerto.</param>
     public void EstablecerPuerto(string puerto)
     {
         networkPort = int.Parse(puerto);
     }
 
+    /// <summary>
+    /// Establece el nombre de la partida.
+    /// </summary>
+    /// <param name="nombre">El nombre.</param>
     public void EstablecerNombrePartida(string nombre)
     {
         serverName = nombre;
     }
 
+    /// <summary>
+    /// Desconecta al jugador de la partida.
+    /// </summary>
     public void DesconectarDePartida()
     {
         if (NetworkServer.connections.Count > 0)
