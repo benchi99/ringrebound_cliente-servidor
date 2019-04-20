acd099e2e6bab2757cf2639b8c126eac539f4ffe
Rubén Bermejo Romero <mejoruben@gmail.com>
2019-04-20 11:52:33 +0200

    feature: Launcher script created.

---
+++ Assets/Scripts/Levels/Launcher.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Launcher : NetworkBehaviour
+{
+    #region Variables
+
+    [SerializeField] private float fuerza = 5;
+    [SerializeField] private float dirEjeY = 10;
+
+    #endregion
+
+    #region Métodos Unity
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+            Lanzar(other.gameObject);
+    }
+
+    #endregion
+
+    #region Otros métodos
+
+
+    void Lanzar(GameObject gameObject)
+    {
+        var jug = gameObject.GetComponent<Jugador>();
+        Vector3 launchVector = new Vector3(transform.forward.x, dirEjeY, transform.forward.z);
+
+        jug.Lanzar(launchVector, fuerza);
+    }
+    
+    #endregion
+}
